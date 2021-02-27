d569eb920de877106407948e7a328b74987290b4
Keith <keithwhisper@gmail.com>
2021-02-27 19:46:36 +0800

    feat: update style of block and tx page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       colors: {
         primary: '#6C2AF6',
         secondary: '#9A5FE4',
-        "plain-grey": '#666666',
+        "light-grey": "#CCCCCC",
+        "dark-grey": '#666666',
         "tag-primary": "#F69E4C",
         "tag-secondary": "#FFF1DE"
       },
@@ -17,7 +18,8 @@ module.exports = {
     extend: {
       backgroundColor: ['odd', 'even', 'hover', 'focus'],
       borderColor: ['hover', 'focus'],
-      padding: ['odd', 'even'],
+      padding: ['odd', 'even', 'first', 'last'],
+      borderWidth: ['last'],
     },
   },
   plugins: [],
